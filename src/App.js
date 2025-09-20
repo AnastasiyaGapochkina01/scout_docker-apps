2bdd9ae111331a66f1ec8b460c0cdf6d4fedc29d
anestesia <anastasiya.gapochkina01@yandex.ru>
2025-09-20 08:41:31 +0300

    Add react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from React + Nginx!</h1>
+      <p>This app is served by nginx from a Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
+
